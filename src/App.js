668346d22a42945ba58c0e1b1168a235f9909f19
Vivek Anjan <vivekanjankc12345@gmail.com>
2022-11-27 14:35:30 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
-
+import {BrowserRouter, Route,Routes} from "react-router-dom"
+import Mainhome from "./MainHome"
+import Home from './Home';
+import About from './About';
+import Contact from './Contact';
+import Project from './Project';
+import Skill from './Skill';
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+  <Routes>
+        <Route path='/' element={<Mainhome />} >
+        <Route index  element={<Home />} />
+        <Route path='/About' element={<About/>} />
+        <Route path='/Contact' element={<Contact />} />
+        <Route path='/Skill' element={<Skill/>} />
+        <Route path='/Project' element={<Project />} />
+        
+        </Route>
+  </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import About from "./About"
+const Home = () => {
+  return (
+    <div>
+      <About />
+    </div>
+  )
+}
+
+export default Home
